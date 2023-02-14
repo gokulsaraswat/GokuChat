3684f51bc6c2aa448d822d563f3d86e3b7e0709a
gokul saraswat <gokulsaraswat07@gmail.com>
2023-02-14 21:05:56 +0530

    Chat Application Porototype Completed

---
+++ app/src/main/java/com/example/gokuchat/firebase/MessegingService.java
@@ -0,0 +1,22 @@
+package com.example.gokuchat.firebase;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessegingService extends FirebaseMessagingService {
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+        Log.d("FCM" , "s");
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.d("FCM" , "Message" + remoteMessage.getNotification().getBody());
+    }
+}
---
+++ app/src/main/java/com/example/gokuchat/listeners/UserListener.java
@@ -0,0 +1,7 @@
+package com.example.gokuchat.listeners;
+
+import com.example.gokuchat.models.User;
+
+public interface UserListener {
+    void onUserClicked(User user);
+}
